4004c5ab0173c034acb0f7e63a22852a977f5c2b
Sushil Behera <sbehera@mpgbis.com>
2023-09-11 16:07:51 +0530

    update

--- pages/_document.js
+++ pages/_document.js
@@ -43,8 +43,8 @@ export default function Document() {
         <body>
           <Main />
           <NextScript />
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-          <script src="https://code-eu1.jivosite.com/widget/0vnMaZXeaH" async></script>
+            {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+            <script src="https://code-eu1.jivosite.com/widget/0vnMaZXeaH" async></script> */}
         </body>
       </Html>
   )
