03a825632b9c80dda0b195dd2aa4a201b8de716e
Sushil Behera <sbehera@mpgbis.com>
2023-09-11 16:37:29 +0530

    update script

--- pages/_document.js
+++ pages/_document.js
@@ -44,7 +44,7 @@ export default function Document() {
         <body>
           <Main />
           <NextScript />
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"defer></script>
+          <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"  strategy="defer" />
           <Script src="https://code-eu1.jivosite.com/widget/0vnMaZXeaH"  strategy="defer" />
         </body>
       </Html>
